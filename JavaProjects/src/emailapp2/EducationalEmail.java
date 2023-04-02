4f0eb7091fde62537e536ee39c8d2bae230b667c
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-01 23:26:55 -0300

    Increase mailbox capacity method

--- JavaProjects/src/emailapp2/EducationalEmail.java
+++ JavaProjects/src/emailapp2/EducationalEmail.java
@@ -29,7 +29,12 @@ public class EducationalEmail implements Email{
 
     @Override
     public void updateMailboxCapacity() {
-
+        Integer newMailboxCapacity;
+        System.out.println("Enter your new mailbox capacity:");
+        Scanner scanner = new Scanner(System.in);
+        newMailboxCapacity = scanner.nextInt();
+        this.mailboxCapacity = newMailboxCapacity;
+        System.out.println("Mailbox capacity increased");
     }
 
     public EducationalEmail() {
